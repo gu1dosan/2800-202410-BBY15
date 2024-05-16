053c5f35969b443d350018e2636dd416f0d1e5d8
Steven Ly <steven.ly236@gmail.com>
2024-05-16 10:00:06 -0700

    Secondary Design to Event Submission page.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const Joi = require("joi");
-const {ObjectId} = require("mongodb");
+const { ObjectId } = require("mongodb");
 
 const expireTime = 60 * 60 * 1000; //expires after 1 hour (minutes * seconds * millis)
 
@@ -22,28 +22,28 @@ const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const mongodb_database = process.env.MONGODB_DATABASE;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 var jsonParser = bodyParser.json()
 
-var {database} = include('databaseConnection');
+var { database } = include('databaseConnection');
 
 const userCollection = database.db(mongodb_database).collection('users');
 
 var mongoStore = MongoStore.create({
-	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}`,
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}`,
     collectionName: 'sessions',
     crypto: {
-		secret: mongodb_session_secret
-	}
+        secret: mongodb_session_secret
+    }
 })
 
 app.set('view engine', 'ejs');
 
-app.use(session({ 
+app.use(session({
     secret: node_session_secret,
-	store: mongoStore, //default is memory store 
-	saveUninitialized: false, 
-	resave: true
+    store: mongoStore, //default is memory store 
+    saveUninitialized: false,
+    resave: true
 }));
 
 function isValidSession(req) {
@@ -53,7 +53,7 @@ function isValidSession(req) {
     return false;
 }
 
-function sessionValidation(req,res,next) {
+function sessionValidation(req, res, next) {
     if (isValidSession(req)) {
         next();
     }
@@ -72,7 +72,7 @@ function isAdmin(req) {
 function adminAuthorization(req, res, next) {
     if (!isAdmin(req)) {
         res.status(403);
-        res.render("errorMessage", {error: "403 - Not Authorized"});
+        res.render("errorMessage", { error: "403 - Not Authorized" });
         return;
     }
     else {
@@ -80,15 +80,16 @@ function adminAuthorization(req, res, next) {
     }
 }
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.locals.session = req.session;
     next();
 });
 app.get('/', (req, res) => {
-    res.render('index',{session:req.session});
+    res.render('index', { session: req.session });
 });
 
 app.get('/test', (req, res) => {
+    const successMessage = req.query.success === 'true' ? 'Event added successfully' : null;
     res.render('event_submission');
 });
 
@@ -101,48 +102,66 @@ app.get('/test', (req, res) => {
 //     res.render('event_submission');
 // });
 
-// app.post('/test', async (req,res) => {
-//     var title = req.body.eventTitle;
-//     var description = req.body.description;
-//     var location = req.body.location;
-//     var info = req.body.contactInfo;
-//     var category = req.body.category;
+app.post('/test', async (req, res) => {
+    var title = req.body.eventTitle;
+    var description = req.body.description;
+    var location = req.body.location;
+    var info = req.body.contactInfo;
+    var category = req.body.category;
 
-//     if (!title || !category) {
-//         res.send("You must provide a title and category<br/> <a href='/test'>Try again</a>");
-//         return
-//     }
+    if (!title || !category) {
+        res.send("You must provide a title and category<br/> <a href='/test'>Try again</a>");
+        return
+    }
 
-//     const schema = Joi.object(
-//         {
-//             title: Joi.string().max(50).required(),
-//             description: Joi.string().max(500),
-//             location: Joi.string().max(50),
-//             info: Joi.string().max(50),
-//         });
-    
-//     const validationResult = schema.validate({title, description, location, info});
+    const schema = Joi.object(
+        {
+            title: Joi.string().max(50).required(),
+            description: Joi.string().max(500),
+            location: Joi.string().max(50),
+            info: Joi.string().max(50),
+        });
 
-//     if (validationResult.error != null) {
-//         console.log(validationResult.error);
-//         res.send("/test");
-//         return;
-//     }
+    const validationResult = schema.validate({ title, description, location, info });
+
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.send("/test");
+        return;
+    }
+
+    const newEvent = {
+        title: title,
+        description: description,
+        location: location,
+        info: info,
+        category: category,
+        submittedBy: req.session.user_ID
+    }
+
+    await database.db(mongodb_database).collection('groups').updateOne(
+        { _id: ObjectId(groupId) }, // need some way to get the group id
+        { $push: { events: newEvent } }
+    );
+
+    console.log("Event added");
+    res.redirect('/test?success=true');
+});
 
 
 
 app.get('/signup', (req, res) => {
-    if(req.session.authenticated) {
+    if (req.session.authenticated) {
         res.redirect('/');
     } else {
-        var missing={};
+        var missing = {};
         missing.name = req.query.missingName;
         missing.email = req.query.missingEmail;
         missing.password = req.query.missingPassword;
-        res.render('signup',{missing:missing});
+        res.render('signup', { missing: missing });
     }
 });
-app.post('/signup', async (req,res) => {
+app.post('/signup', async (req, res) => {
     var name = req.body.name;
     var email = req.body.email;
     var password = req.body.password;
@@ -154,17 +173,17 @@ app.post('/signup', async (req,res) => {
             email: Joi.string().email().required(),
             password: Joi.string().max(20).required()
         });
-        
-        const validationResult = schema.validate({name, email, password});
+
+        const validationResult = schema.validate({ name, email, password });
         if (validationResult.error != null) {
-           console.log(validationResult.error);
-           res.send(`${validationResult.error.message}<br/> <a href='/signup'>Try again</a>`);
-           return;
-       }
-    
+            console.log(validationResult.error);
+            res.send(`${validationResult.error.message}<br/> <a href='/signup'>Try again</a>`);
+            return;
+        }
+
         var hashedPassword = await bcrypt.hash(password, saltRounds);
-        
-        await userCollection.insertOne({name: name, email:email, user_type: 'user', password: hashedPassword});
+
+        await userCollection.insertOne({ name: name, email: email, user_type: 'user', password: hashedPassword });
         req.session.authenticated = true;
         req.session.email = email;
         req.session.name = name;
@@ -174,73 +193,73 @@ app.post('/signup', async (req,res) => {
 });
 
 app.get('/login', (req, res) => {
-    if(req.session.authenticated) {
+    if (req.session.authenticated) {
         res.redirect('/');
     } else {
         res.render('login');
     }
 });
-app.post('/login', async (req,res) => {
+app.post('/login', async (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
     const schema = Joi.string().email().required();
-	const validationResult = schema.validate(email);
-	if (validationResult.error != null) {
-	   console.log(validationResult.error);
-	   res.send("You must provide a username and password<br/> <a href='/login'>Try again</a>");
-	   return;
-	}
+    const validationResult = schema.validate(email);
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.send("You must provide a username and password<br/> <a href='/login'>Try again</a>");
+        return;
+    }
 
-	const result = await userCollection.find({email: email}).project({email: 1, name:1, user_type:1, password: 1, _id: 1}).toArray();
+    const result = await userCollection.find({ email: email }).project({ email: 1, name: 1, user_type: 1, password: 1, _id: 1 }).toArray();
 
-	// console.log(result);
-	if (result.length != 1) {
-		// console.log("user not found");
-		res.send("Invalid email/password combination<br/> <a href='/login'>Try again</a>");
-		return;
-	}
-	if (await bcrypt.compare(password, result[0].password)) {
-		// console.log("correct password");
-		req.session.authenticated = true;
+    // console.log(result);
+    if (result.length != 1) {
+        // console.log("user not found");
+        res.send("Invalid email/password combination<br/> <a href='/login'>Try again</a>");
+        return;
+    }
+    if (await bcrypt.compare(password, result[0].password)) {
+        // console.log("correct password");
+        req.session.authenticated = true;
         req.session.email = email;
         req.session.name = result[0].name;
         req.session.user_type = result[0].user_type;
         req.session.cookie.maxAge = expireTime;
 
-		res.redirect('/members');
-		return;
-	} else {
-		// console.log("incorrect password");
-		res.send("Invalid email/password combination<br/> <a href='/login'>Try again</a>");
-		return;
-	}
+        res.redirect('/members');
+        return;
+    } else {
+        // console.log("incorrect password");
+        res.send("Invalid email/password combination<br/> <a href='/login'>Try again</a>");
+        return;
+    }
 });
 
 app.get('/members', sessionValidation, (req, res) => {
-        res.render('members',{session:req.session});
+    res.render('members', { session: req.session });
 });
 
-app.get('/admin', sessionValidation, adminAuthorization , async (req,res) => {
+app.get('/admin', sessionValidation, adminAuthorization, async (req, res) => {
     const result = await userCollection.find().toArray();
     // console.log(result)
-    res.render("admin", {users: result});
+    res.render("admin", { users: result });
 });
 
-app.post('/promoteToAdmin', sessionValidation, adminAuthorization, jsonParser, async (req,res) => {
+app.post('/promoteToAdmin', sessionValidation, adminAuthorization, jsonParser, async (req, res) => {
     // console.log(req.body)
     // console.log("promoting " + req.body.id + " to admin")
-    const result = await userCollection.updateOne({_id: new ObjectId(req.body.id)}, {$set: {user_type: 'admin'}});
+    const result = await userCollection.updateOne({ _id: new ObjectId(req.body.id) }, { $set: { user_type: 'admin' } });
     // console.log(result)
-    res.send({success: true});
+    res.send({ success: true });
 });
-app.post('/demoteToUser', sessionValidation, adminAuthorization, jsonParser, async (req,res) => {
-    await userCollection.updateOne({_id: new ObjectId(req.body.id)}, {$set: {user_type: 'user'}});
-    res.send({success: true});
+app.post('/demoteToUser', sessionValidation, adminAuthorization, jsonParser, async (req, res) => {
+    await userCollection.updateOne({ _id: new ObjectId(req.body.id) }, { $set: { user_type: 'user' } });
+    res.send({ success: true });
 });
 
 app.get('/logout', (req, res) => {
-    if(req.session.authenticated) {
+    if (req.session.authenticated) {
         req.session.destroy();
         res.redirect("/");
     } else
@@ -249,9 +268,9 @@ app.get('/logout', (req, res) => {
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("*", (req,res) => {
-	res.status(404);
-	res.render("404");
+app.get("*", (req, res) => {
+    res.status(404);
+    res.render("404");
 })
 
 app.listen(port, () => {
