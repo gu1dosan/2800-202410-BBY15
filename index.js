5c2653ee76db70b3415be8e90cb8aba389a7922f
Victor Wong <victor_wong444@hotmail.com>
2024-05-29 16:30:39 -0700

    corner case for case where no selected activities exist

--- index.js
+++ index.js
@@ -566,7 +566,7 @@ app.get("/activities", sessionValidation, async (req, res) => {
 
     // If no groups found, return a 404 status
     if (!groups.length) {
-      return res.status(404).send("No selected events found for the user.");
+      res.render("errorMessage", { msg: "No activities existt for the group" });
     }
 
     // Extract the selected events from each group
